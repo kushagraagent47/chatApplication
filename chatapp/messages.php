198d130ea46f35fe19d486c47ce02deb92d41491
kushagraagent47 <kushagraagent47@gmail.com>
2018-10-26 20:49:11 +0530

    Initial commit

---
+++ chatapp/messages.php
@@ -0,0 +1,12 @@
+
+<meta http-equiv="refresh" content="5">
+
+<?php
+
+require_once('inc/chat.inc.php');
+
+$oSimpleChat = new SimpleChat();
+
+echo $oSimpleChat->getMessages();
+
+?>
---
+++ db.php
@@ -0,0 +1,17 @@
+<?php
+
+
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db_name = "participants";
+
+    $con = new mysqli($host,$user,$pass,$db_name);
+
+    function formatDate($date)
+    {
+        return date('g:i a',strtotime($date));
+    }
+
+
+?>
